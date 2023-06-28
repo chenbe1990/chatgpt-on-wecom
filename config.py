19933c3b80ecbcbbe432ec39c18b16e61b43cf27
Administrator <344248456@qq.com>
2023-06-28 17:55:58 +0800

    1.新增自定义数据库 2.数据库容错

--- config.py
+++ config.py
@@ -104,7 +104,11 @@ available_setting = {
     # 知识库平台配置
     "use_linkai": False,
     "linkai_api_key": "",
-    "linkai_app_code": ""
+    "linkai_app_code": "",
+    "host" : "",
+    "user" : "",
+    "password" : "",
+    "database" : ""
 }
 
 
--- db.py
+++ db.py
@@ -1,21 +1,35 @@
 import datetime
 import pymysql.connections
+from config import conf, load_config
+from common.log import logger
+
 
 class Database:
     def __init__(self):
-        self.conn = pymysql.connect(
-            host="localhost",
-            user="root",
-            password="84789791",
-            database="chat"
-        )
-        self.cursor = self.conn.cursor()
+        load_config()
+        self.conn = None
+        self.cursor = None
+        try:
+            self.conn = pymysql.connect(
+                host=conf().get("host"),
+                user=conf().get("user"),
+                password=conf().get("password"),
+                database=conf().get("database")
+            )
+            self.cursor = self.conn.cursor()
+        except Exception as e:
+            logger.warn(f"连接数据库时出错: {str(e)}")
 
-    def insert_chatlog(self,user_id,req_content,res_content,prompt_tokens,completion_tokens,total_tokens):
-        sql = "INSERT INTO chat_wecom (user_id,req_content,res_content,prompt_tokens,completion_tokens,total_tokens,created_at) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-        values = (user_id, req_content,res_content,prompt_tokens,completion_tokens,total_tokens,datetime.datetime.now())
-        self.cursor.execute(sql, values)
-        self.conn.commit()
+    def insert_chatlog(self, user_id, req_content, res_content, prompt_tokens, completion_tokens, total_tokens):
+        if not self.conn or not self.cursor:
+            logger.warn("无法执行SQL语句，数据库连接未成功建立")
+            return
 
-
+        try:
+            sql = "INSERT INTO chat_wecom (user_id,req_content,res_content,prompt_tokens,completion_tokens,total_tokens,created_at) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+            values = (user_id, req_content, res_content, prompt_tokens, completion_tokens, total_tokens, datetime.datetime.now())
+            self.cursor.execute(sql, values)
+            self.conn.commit()
+        except Exception as e:
+            logger.warn(f"插入聊天记录时出错: {str(e)}")
 
